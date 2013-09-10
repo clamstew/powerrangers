414c84446b7b53a018c52385e186f233c1dcbb0f
Clay Stewart <claytonmstewart@gmail.com>
2013-09-10 15:55:35 -0500

    Build methods for Person Class

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,23 @@
 class Person
+  def initialize name
+    @name
+    @caffeine_level = 0
+    @strength = 100
+  end
+
+  def run miles
+  	@strength = @strength - (10 * miles)
+  end
+
+  def scream
+  	@strength -= 1
+  	puts "Ahhhhhh!!"
+  end
+
+  def drink_coffee
+  	@caffeine_level += 1
+  	@strength += 1
+  end
 end
 
 class PowerRanger
